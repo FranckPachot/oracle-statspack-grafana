1a48ad609b91cf047794bb15cef2a4e7f186e5bb
Franck Pachot <statspack@pachot.net>
2021-06-02 15:15:53 +0000

    psql connect

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ podman run -d --name franck oracle_fdw
 echo "=== log"
 until podman logs franck | grep -C 999 "listening on IPv4 address" ; do sleep 1; done
 echo "=== check"
-podman exec -t franck psql -e -c "select version();"
+podman exec -t franck psql -e -c "select version();" | grep "compiled by gcc" && git commit -am "psql connect" && git push
 )
