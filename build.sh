356c283e099d9b8b33a585106173b5866eb3689b
Franck Pachot <statspack@pachot.net>
2021-06-02 15:14:26 +0000

    psql connect

---
+++ build.sh
@@ -0,0 +1,12 @@
+(
+cd FDW_build
+podman rm -f franck 
+echo "=== build"
+podman build -t oracle_fdw . 
+echo "=== run"
+podman run -d --name franck oracle_fdw
+echo "=== log"
+until podman logs franck | grep -C 999 "listening on IPv4 address" ; do sleep 1; done
+echo "=== check"
+podman exec -t franck psql -e -c "select version();"
+)
