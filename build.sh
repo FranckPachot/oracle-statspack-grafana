a7d2d8039a4e793be98f6c7d272d43b035567e82
root <root@franckdemomle.backupsubnetad1.cernvcn.oraclevcn.com>
2021-06-08 14:25:08 +0000

    add grafana

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 (
 cd oracle-perfstat-fdw
-sudo podman rm -f franck 
+sudo docker rm -f franck 
 echo "=== build"
-sudo podman build -t oracle-perfstat-fdw .  ||  exit
+sudo docker build -t oracle-perfstat-fdw .  ||  exit
 echo "=== images"
-sudo podman images
+sudo docker images
 echo "=== run"
-sudo podman run -d --name franck -e POSTGRESQL_ADMIN_PASSWORD=statspack oracle-perfstat-fdw
+sudo docker run -d --name franck -e POSTGRESQL_ADMIN_PASSWORD=statspack oracle-perfstat-fdw
 echo "=== log"
 sleep 5
-sudo podman logs franck
-until sudo podman logs franck | grep -C 999 "listening on IPv4 address" ; do sleep 1; done
+sudo docker logs franck
+until sudo docker logs franck | grep -C 999 "listening on IPv4 address" ; do sleep 1; done
 echo "=== check"
-sudo podman exec franck psql -e <<'SQL'
-call  ora$perf$define_perfstat_fdw('pdb1','//database-1.clw0gescgpyk.us-east-1.rds.amazonaws.com:1521/pdb1','perfstat');
+sudo docker exec franck psql -e <<'SQL'
+ select  oraperfstat$define_fdw('pdb1','//localhost:1521/DEV15','PERFSTAT','password',true,true);
+ select * from oraperfstat$available_servers();
+ explain analyze select * from system_events;
 SQL
 git commit -am "psql connect" && git push
-sudo podman tag oracle-perfstat-fdw pachot/oracle-perfstat-fdw
-sudo podman login docker.io
-sudo podman push pachot/oracle-perfstat-fdw
+sudo docker tag oracle-perfstat-fdw pachot/oracle-perfstat-fdw
+sudo docker login docker.io
+sudo docker push pachot/oracle-perfstat-fdw
 )
